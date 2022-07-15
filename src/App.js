617fcbd717a0ab86f20a7ff79813b1f7a220679c
Brian Harsha <harshabc4@gmail.com>
2022-07-15 16:52:45 -0400

    working

--- src/App.js
+++ src/App.js
@@ -4,17 +4,23 @@ import Navbar from "./components/Navbar.js";
 import Square from "./components/Square.js";
 
 function App() {
-  const [gameBoard, setGameBoard] = React.useState(startBoard);
+  const [gameBoard, setGameBoard] = React.useState(
+    Array.from({ length: 16 }, createSquare)
+  );
+  // const [gameBoard, setGameBoard] = React.useState(startBoard);
+
   // const [gameBoard, setGameBoard] = React.useState(Array.from({ length: 6 }));
 
-  const [squareColor, setSquareColor] = React.useState("");
+  // const [squareColor, setSquareColor] = React.useState("");
 
-  const styles = { background: squareColor };
+  // const styles = { background: squareColor };
 
   const boardElements = gameBoard.map((square) => (
     <Square
-      style={styles}
-      handleMouseEnter={() => setSquareColor("green")}
+      // style={styles}
+      // handleMouseEnter={() => setSquareColor("green")}
+      isHeld={square.isHeld}
+      handleMouseEnter={() => mouseOver(square.id)}
       // handleMouseEnter={mouseOver}
     />
   ));
@@ -42,13 +48,13 @@ function App() {
   //   setGameBoard(Array.from({ length: event.target.value }));
   // }
 
-  // function mouseOver(id) {
-  //   setSquareColor((oldDice) =>
-  //     oldDice.map((die) => {
-  //       return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
-  //     })
-  //   );
-  // }
+  function mouseOver(id) {
+    setGameBoard((oldDice) =>
+      oldDice.map((die) => {
+        return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
+      })
+    );
+  }
 
   return (
     <main>
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,9 +1,13 @@
 import React from "react";
 
 function Square(props) {
+  const styles = {
+    backgroundColor: props.isHeld ? "blue" : "brown",
+  };
   return (
     <div
-      style={props.style}
+      style={styles}
+      //   style={props.style}
       className="square-wrapper"
       onMouseEnter={props.handleMouseEnter}
     >
